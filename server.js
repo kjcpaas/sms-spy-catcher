3abe8c3bdf5e0b265bab868efab400411d497193
Joy Paas <kjcpaas@gmail.com>
2016-08-08 13:53:51 +0800

    initial setup

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express')
+
+var app = new express()
+var port = 8020
+
+app.get('/', function(req, res) {
+   res.sendFile(__dirname + '/public/index.html')
+})
+
+app.listen(port, function(error) {
+  if (error) {
+    console.error(error)
+  } else {
+    console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port)
+  }
+})
